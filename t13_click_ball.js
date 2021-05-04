d53d8eb6ef3ccdf2d35d0b1e1fe1d584019c708f
Logan Curtis <>
2021-05-04 02:14:50 +0000

    ball hit

--- t13_click_ball.js
+++ t13_click_ball.js
@@ -3,25 +3,27 @@
 /********************************************************/
 
 var ballsArray = [];
-const NUMBALLS = 1
-var hits = 0 
-
+const NUMBALLS = 2
+var hits = 0
+var misses = 0
+//var ballHit= false
 
 function setup() {
-  createCanvas(350,350)
+  createCanvas(350, 350)
   console.log("startGame");
   for (var i = 0; i < NUMBALLS; i++) {
     ballsArray[i] = {
+      
       x: 60,
       y: 20,
       dia: 50,
-     // speedX:   random(2,10),
+      // speedX:   random(2,10),
       //speedY:   random(2,10),
-      speedX : 3,
-      speedY : 3,
-      r: random(50,150),
-      g: random(50,150),
-      b: random(50,150),
+      speedX: 3,
+      speedY: 3,
+      r: random(50, 150),
+      g: random(50, 150),
+      b: random(50, 150),
 
       display: function () {
         fill(this.r, this.g, this.b)
@@ -57,36 +59,56 @@ function setup() {
         this.y = random(150, width - this.dia / 2)
         this.x = random(150, width - this.dia / 2)
       },
-      
 
+      clicked: function () {
+        var d = dist(mouseX, mouseY, this.x, this.y)
+        if (d<this.dia /2) {
+          hits++;
+          console.log("hits:"+hits)
+         // ballHit = true
+          //console.log(ballHit);
+        }
+        else{
+          misses++;
+          console.log("misses:"+misses)
+        }
+
+      }
 
     }
     ballsArray[i].place();
+
   }
 
-  
+
 };
 
-
-
-function hit(){
- var px2Ball = dist(ballsArray[0].x, ballsArray[0].y, mouseX, mouseY);
-
-  if(px2Ball<ballsArray.dia){
-    hits++;
- console.log(hits)
+function mousePressed() {
+  for (var i = 0; i < ballsArray.length; i++) {
+    ballsArray[i].clicked();
 
   }
 
 }
 
+//function hit(){
+
+//var px2Ball = dist(ballsArray[0].x, ballsArray[0].y, mouseX, mouseY);
+
+// if(px2Ball<ballsArray.dia){
+//   hits++;
+// console.log(hits)
+
+// }
+
+//}
+
 function draw() {
   background(220);
-  for (var i = 0;  i <ballsArray.length ; i++) {
+  for (var i = 0; i < ballsArray.length; i++) {
     ballsArray[i].display();
     ballsArray[i].move();
     ballsArray[i].bounce();
-   
   }
 
 }
